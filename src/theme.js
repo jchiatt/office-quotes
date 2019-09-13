88a055428afb3d9f09d4379b0f8505ae4cb635fc
J.C. Hiatt <jc@jchiatt.com>
2019-09-13 10:12:10 -0500

    styles(global): initialize theme with emotion

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export default {
+  breakpoints: {
+    tablet: "768px",
+    desktop: "1024px",
+  },
+  colors: {
+    primary: "#023334",
+    black: "#171717",
+    white: "#fdfdfd",
+  },
+  maxWidth: "1000px",
+}
