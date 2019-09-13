045214f1691652e70a894e41c42e78e39950624c
J.C. Hiatt <jc@jchiatt.com>
2019-09-13 08:59:47 -0500

    styles(fonts): add gatsby-plugin-preftech-google-fonts and install Montserrat

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,15 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Montserrat`,
+            variants: [`400`, `700`, `900`],
+          },
+        ],
+      },
+    },
+  ],
 }
